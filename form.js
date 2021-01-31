c405f94d42e509aac31d777ae80598c9cfd794eb
hkarthikraj01 <53332719+hkarthikraj01@users.noreply.github.com>
2021-01-31 13:51:42 +0530

    Update form.js

--- form.js
+++ form.js
@@ -43,6 +43,10 @@ function codeverify() {
         alert(error.message);
     });
 }
+
+
+
+
 const form = document.querySelector('#create-form');
 var y = document.getElementById("ve");
 form.addEventListener('submit', (e) => {
@@ -61,6 +65,18 @@ form.addEventListener('submit', (e) => {
     form.place.value= '';
     form.Date.value= '';
     form.msg.value= '';
+        var strText = document.getElementById("full_name").value;          
+        var strText1 = document.getElementById("phone").value;
+        var strText2 = document.getElementById("place").value;
+        var strText3 = document.getElementById("Date").value;
+        var strText4 = document.getElementById("msg").value;
+        var result = 'Name:  '+strText + '%0APhone Number: ' + strText1+'%0APlace: ' + strText2+'%0ADate: ' + strText3+'%0AMessage: ' + strText4;
+        var finalMsg = encodeURI(result);
+         document.getElementById("create-form").addEventListener("submit", (e) => {
+            e.preventDefault();
+            const request = new XMLHttpRequest();
+        const url = 'https://api.telegram.org/bot1673038851:AAEIqwAgS-TtA331gOhUHVhRnVJf_34bUsE/sendMessage?chat_id=-1001225437691&text='+result;
+        }
     }
     if(y.style.display === "none")
     {
@@ -68,3 +84,5 @@ form.addEventListener('submit', (e) => {
     }
 });
 
+
+       
