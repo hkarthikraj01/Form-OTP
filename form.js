d93fe56f9a7f6a0852acb66d1c21c289ef76b5eb
hkarthikraj01 <53332719+hkarthikraj01@users.noreply.github.com>
2021-01-31 19:15:24 +0530

    Update form.js

--- form.js
+++ form.js
@@ -19,23 +19,7 @@ form.addEventListener('submit', (e) => {
     form.Date.value= '';
     form.msg.value= '';     
         
-    var strText = document.getElementById("full_name").value;          
-        var strText1 = document.getElementById("phone").value;
-        var strText2 = document.getElementById("place").value;
-        var strText3 = document.getElementById("Date").value;
-        var strText4 = document.getElementById("msg").value;
-        var result = 'Name:  '+strText + '%0ANumber: ' + strText1+'%0APlace: ' + strText2+'%0ADate: ' + strText3+'%0AMessage: ' + strText4;
-        var finalMsg = encodeURI(result);
-        document.getElementById("create-form").addEventListener("submit", (e) => {
-        e.preventDefault();
-        {
-        const request = new XMLHttpRequest();
-        const url = 'https://api.telegram.org/bot1673038851:AAEIqwAgS-TtA331gOhUHVhRnVJf_34bUsE/sendMessage?chat_id=-1001225437691&text='+result;
-        request.open("post", url);
-        request.send();
-        }
-        });    
-        
+  
     alert("Thank For Register");
     }
     
