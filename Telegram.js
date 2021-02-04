5aa88ccf61ec1cfa42653156f92b0ff850df8474
hkarthikraj01 <53332719+hkarthikraj01@users.noreply.github.com>
2021-02-04 14:05:01 +0530

    Update Telegram.js

--- Telegram.js
+++ Telegram.js
@@ -5,7 +5,7 @@
         var strText3 = document.getElementById("Date").value;
         var strText4 = document.getElementById("msg").value;
         var y = document.getElementById("ve");
-        var result = 'Name:  '+strText + '%0ANumber: ' + strText1+'%0APlace: ' + strText2+'%0ADate: ' + strText3+'%0AMessage: ' + strText4;
+        var result = '%0AName:  '+strText + '%0ANumber: ' + strText1+'%0APlace: ' + strText2+'%0ADate: ' + strText3+'%0AMessage: ' + strText4;
         var finalMsg = encodeURI(result);
         document.getElementById("create-form").addEventListener("submit", (e) => {
         e.preventDefault();
@@ -18,7 +18,7 @@
         const url = 'https://api.telegram.org/bot1673038851:AAEIqwAgS-TtA331gOhUHVhRnVJf_34bUsE/sendMessage?chat_id=-1001225437691&text='+result;    
         request.open("post", url);
         request.send();*/
-        window.open("https://www.fast2sms.com/dev/bulk?authorization=YbdnyBCkGrxZSeTl0vwMqmEastOQLRFPcVpXW1J9Huj6DIoKAhxMguvAoKnTp3FzymwcksZU0GJqhbeI&sender_id=FSTSMS&message=This is "+result+"&language=english&route=p&numbers=8248675218");
+        window.open("https://www.fast2sms.com/dev/bulk?authorization=YbdnyBCkGrxZSeTl0vwMqmEastOQLRFPcVpXW1J9Huj6DIoKAhxMguvAoKnTp3FzymwcksZU0GJqhbeI&sender_id=FSTSMS&message=%0AYour Booking Request Processing "+result+"&language=english&route=p&numbers="+strText1);
             }
          }); 
     }
